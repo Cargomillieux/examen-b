289782301e1626dd8013b33c0aca9c13943e7cbc
Cargomillieux <116322048+Cargomillieux@users.noreply.github.com>
2022-11-24 09:55:29 -0500

    Mise à jour main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 def on_forever():
     servos.P0.set_angle(150)
     infraroug = pins.digital_read_pin(DigitalPin.P1)
-    infraroug = infraroug * 100
     if infraroug == 1:
-
-        basic.show_leds("""
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        """)
-    basic.pause(1000)
-
+        servos.P0.set_angle(60)
+        basic.pause(2000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,16 +1,9 @@
 basic.forever(function on_forever() {
     servos.P0.setAngle(150)
     let infraroug = pins.digitalReadPin(DigitalPin.P1)
-    infraroug = infraroug * 100
     if (infraroug == 1) {
-        basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
+        servos.P0.setAngle(60)
+        basic.pause(2000)
     }
     
-    basic.pause(1000)
 })
